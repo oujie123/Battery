544e488a59d81176c5bc741fbadf22887e377823
jackou <oujie_forwork@foxmail.com>
2020-11-23 14:53:16 +0800

    增加电量监控工具

---
+++ app/src/main/java/com/gacrnd/gcs/battery/battery/UploadWorker.java
@@ -0,0 +1,26 @@
+package com.gacrnd.gcs.battery.battery;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+
+public class UploadWorker extends Worker {
+    private static final String TAG = "UploadWorker";
+
+    public UploadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        // 执行后台任务......
+        Log.e(TAG, "doWork: 执行Work");
+        return Result.success();
+    }
+}
+
